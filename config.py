b21ab7a874a7cd1a2436be569479be8715697204
Johannes Schmidt <johannes.schmidt.vik@gmail.com>
2024-01-29 10:30:20 +0100

    feat: restructure projects

--- config.py
+++ config.py
@@ -1,27 +1,57 @@
+import json
+from enum import Enum
+from functools import cached_property
+from pathlib import Path
 from typing import List, Optional
 
 from pydantic_settings import BaseSettings
 
 
+def read_json_config(path: Path):
+    with open(path, "r") as fh:
+        data: dict = json.load(fh)
+        return data
+
+
+def get_filenames_of_path(path: Path, ext: str = "*") -> List[Path]:
+    """
+    Returns a list of files in a directory/path. Uses pathlib.
+    """
+    filenames = [file for file in path.glob(ext) if file.is_file()]
+    assert len(filenames) > 0, f"No files found in path: {path}"
+    return filenames
+
+
+class InputType(str, Enum):
+    KINESIS: str = "kinesis"
+
+
+class OutputType(str, Enum):
+    POSTGRESQL: str = "postgresql"
+    KINESIS: str = "kinesis"
+
+
 class Config(BaseSettings):
+    # AWS Account
     ACCOUNT_ID: str
     REGION: str
 
     # CONFIGS
-    CONFIG_PATH: str
+    CONFIG_PATH: str  # location of the configs directory
 
-    # Kinesis
-    KINESIS_ARN_INPUT: str
-    KINESIS_ARN_OUTPUT: str
+    # Input
+    INPUT_TYPE: InputType
+
+    # Output
+    OUTPUT_TYPE: OutputType
 
     # AppConfig
     APP_CONFIG_NAME: str
     APP_CONFIG_ENV_NAME: str
-    APP_CONFIG_DEPLOYMENT_STRAT_NAME: str
+    APP_CONFIG_DEPLOYMENT_STRATEGY_NAME: str
 
-    # SECRETS
+    # Secrets
     SECRETS: List[str]
-    SECRET_POSTGRESQL: str
 
     # Lambda
     FUNCTION_NAME: str = "LambdaFunctionKinesis"
@@ -32,6 +62,39 @@ class Config(BaseSettings):
     FUNCTION_VPC: Optional[str] = None
     FUNCTION_ENV: Optional[dict] = None
 
-    # Docker
+    # Docker (optional)
     DOCKER_IMAGE_REPO: Optional[str] = None
     DOCKER_IMAGE_TAG: Optional[str] = None
+
+    @cached_property
+    def input_config_path(self) -> Path:
+        return Path(self.CONFIG_PATH) / "input.json"
+
+    @cached_property
+    def output_config_path(self) -> Path:
+        return Path(self.CONFIG_PATH) / "output.json"
+
+    @cached_property
+    def input_config_data(self) -> dict:
+        data: dict = read_json_config(self.input_config_path)
+        self.validate_input_config_data()
+        return data
+
+    @cached_property
+    def output_config_data(self) -> dict:
+        data: dict = read_json_config(self.output_config_path)
+        self.validate_output_config_data()
+        return data
+
+    def validate_output_config_data(self):
+        ...
+
+    def validate_input_config_data(self):
+        ...
+
+    @cached_property
+    def schema_paths(self):
+        files: List[Path] = get_filenames_of_path(
+            path=Path(self.CONFIG_PATH) / "schemas", ext="*.json"
+        )
+        return files
--- lambda_stack.py
+++ lambda_stack.py
@@ -22,35 +22,21 @@ from aws_cdk.aws_lambda import (
 )
 from aws_cdk.aws_lambda_event_sources import KinesisEventSource
 from aws_cdk.aws_secretsmanager import ISecret, Secret
-from config import Config
+from config import Config, InputType, OutputType, read_json_config
 from constructs import Construct
-
-
-def get_filenames_of_path(path: Path, ext: str = "*") -> List[Path]:
-    """
-    Returns a list of files in a directory/path. Uses pathlib.
-    """
-    filenames = [file for file in path.glob(ext) if file.is_file()]
-    assert len(filenames) > 0, f"No files found in path: {path}"
-    return filenames
-
-
-def read_json_config(path: Path):
-    with open(path, "r") as fh:
-        data: dict = json.load(fh)
-        return data
 
 
 class LambdaStack(Stack):
     """
-    Creates an AWS Lambda Function (Docker image) with AWS Kinesis Stream as the event source.
+    Creates an AWS Lambda Function (Docker image) with an event source as input. Default: AWS Kinesis Stream.
 
-    A Docker image is built and pushed to a private ECR. If the ECR does not exist, it is automatically created.
-    If an ECR image is given in the config this will be used otherwise the local Dockerfile is used as the image
+    A Docker image (Dockerfile) is built and pushed to a private ECR. If the ECR does not exist,
+    it is automatically created.
+    Alternatively, if an ECR image is provided in the config this will be used.
 
     The AWS Lambda Function uses AWS AppConfig to retrieve application information.
     For this to work, you either need to add the AWS AppConfig Agent to the Docker image: https://docs.aws.amazon.com/appconfig/latest/userguide/appconfig-integration-lambda-extensions-container-image.html
-    OR use boto3 in your application code.
+    OR use boto3 in your application code. The latter is used here.
 
     In the context of AWS AppConfig, these actions are performed:
     - Creating an application
@@ -59,7 +45,7 @@ class LambdaStack(Stack):
     - Creating a deployment strategy
     - Deploying the configuration
 
-    The AWS lambda function is given the permission to retrieve provided secrets (ARNs) from the secrets manager.
+    The AWS Lambda Function is given the permission to retrieve provided secrets (ARNs) from the AWS Secrets Manager
 
     """
 
@@ -71,17 +57,38 @@ class LambdaStack(Stack):
         self._build()
 
     def _build(self) -> None:
-        environment: dict = (
-            {} if self.config.FUNCTION_ENV is None else self.config.FUNCTION_ENV
+        # create new role for the lambda function
+        lambda_role: Role = Role(
+            self, "LambdaRole", assumed_by=ServicePrincipal("lambda.amazonaws.com")
         )
 
-        # create aws app config
+        # allow lambda function to log
+        lambda_role.add_to_policy(
+            statement=PolicyStatement(
+                effect=Effect.ALLOW,
+                actions=["logs:CreateLogGroup"],
+                resources=[
+                    f"arn:aws:logs:{self.config.REGION}:{self.config.ACCOUNT_ID}:*"
+                ],
+            )
+        )
+
+        lambda_role.add_to_policy(
+            statement=PolicyStatement(
+                effect=Effect.ALLOW,
+                actions=["logs:CreateLogStream", "logs:PutLogEvents"],
+                resources=[
+                    f"arn:aws:logs:{self.config.REGION}:{self.config.ACCOUNT_ID}:log-group:/aws/lambda/{self.config.FUNCTION_NAME}"
+                ],
+            )
+        )
+
+        # aws app config application
         app_config: CfnApplication = CfnApplication(
             self, "ApplicationConfig", name=self.config.APP_CONFIG_NAME
         )
 
-        environment["app_config_app_name"] = app_config.name
-
+        # aws app config env
         app_env: CfnEnvironment = CfnEnvironment(
             self,
             "Environment",
@@ -89,26 +96,39 @@ class LambdaStack(Stack):
             name=self.config.APP_CONFIG_ENV_NAME,
         )
 
-        environment["app_config_environment_name"] = app_env.name
-
+        # aws app config deployment strategy
         app_deployment_strategy: CfnDeploymentStrategy = CfnDeploymentStrategy(
             self,
-            "DeploymentStrategy",
+            "AppConfigDeploymentStrategy",
             deployment_duration_in_minutes=0,
             growth_factor=1.0,
             replicate_to="NONE",
-            name=self.config.APP_CONFIG_DEPLOYMENT_STRAT_NAME,
+            name=self.config.APP_CONFIG_DEPLOYMENT_STRATEGY_NAME,
         )
 
-        destination_path: Path = (
-            Path(self.config.CONFIG_PATH) / "destination-config.json"
+        lambda_role.add_to_policy(
+            statement=PolicyStatement(
+                effect=Effect.ALLOW,
+                actions=[
+                    "appconfig:GetConfiguration",
+                    "appconfig:GetApplication",
+                    "appconfig:GetLatestConfiguration",
+                    "appconfig:StartConfigurationSession",
+                ],
+                resources=[
+                    f"arn:aws:appconfig:*:{self.config.ACCOUNT_ID}:application/*",
+                ],
+            )
         )
 
-        paths: List[Path] = get_filenames_of_path(
-            path=Path(self.config.CONFIG_PATH) / "schemas", ext="*.json"
-        ) + [destination_path]
+        paths: List[Path] = (
+            self.config.schema_paths
+            + [self.config.input_config_path]
+            + [self.config.output_config_path]
+        )
 
         for path in paths:
+            # for each schema and config (input & output), we create & deploy an aws app config profile
             json_data: dict = read_json_config(path)
 
             app_profile: CfnConfigurationProfile = CfnConfigurationProfile(
@@ -131,23 +151,13 @@ class LambdaStack(Stack):
             )
             app_deployment: CfnDeployment = CfnDeployment(
                 self,
-                f"AppDeployment-{path.stem}",
+                f"Deployment-{path.stem}",
                 application_id=app_config.attr_application_id,
                 configuration_profile_id=app_profile.attr_configuration_profile_id,
                 configuration_version=hosted_configuration_version.ref,
                 deployment_strategy_id=app_deployment_strategy.ref,
                 environment_id=app_env.ref,
             )
-
-        # get kinesis reference
-        kinesis: IStream = Stream.from_stream_arn(
-            self, "KinesisEventSource", stream_arn=self.config.KINESIS_ARN_INPUT
-        )
-
-        # create new role for the lambda function
-        lambda_role: Role = Role(
-            self, "LambdaRole", assumed_by=ServicePrincipal("lambda.amazonaws.com")
-        )
 
         # allow lambda function SP to retrieve secrets from the secrets manager
         for secret in self.config.SECRETS:
@@ -156,16 +166,6 @@ class LambdaStack(Stack):
                 self, secret, secret_complete_arn=secret
             )
 
-        # make sure the secret exists
-        secret_postgresql_ref: ISecret = Secret.from_secret_complete_arn(
-            self,
-            self.config.SECRET_POSTGRESQL,
-            secret_complete_arn=self.config.SECRET_POSTGRESQL,
-        )
-
-        environment["postgres_secret_arn"] = self.config.SECRET_POSTGRESQL
-        environment["kinesis_arn_output"] = self.config.KINESIS_ARN_OUTPUT
-
         lambda_role.add_to_policy(
             statement=PolicyStatement(
                 effect=Effect.ALLOW,
@@ -173,58 +173,9 @@ class LambdaStack(Stack):
                 resources=self.config.SECRETS,
             )
         )
-        lambda_role.add_to_policy(
-            statement=PolicyStatement(
-                effect=Effect.ALLOW,
-                actions=["secretsmanager:GetSecretValue"],
-                resources=[self.config.SECRET_POSTGRESQL],
-            )
-        )
 
-        lambda_role.add_to_policy(
-            statement=PolicyStatement(
-                effect=Effect.ALLOW,
-                actions=[
-                    "appconfig:GetConfiguration",
-                    "appconfig:GetApplication",
-                    "appconfig:GetLatestConfiguration",
-                    "appconfig:StartConfigurationSession",
-                ],
-                resources=[
-                    f"arn:aws:appconfig:*:{self.config.ACCOUNT_ID}:application/*",
-                ],
-            )
-        )
-
-        lambda_role.add_to_policy(
-            statement=PolicyStatement(
-                effect=Effect.ALLOW,
-                actions=["logs:CreateLogGroup"],
-                resources=[
-                    f"arn:aws:logs:{self.config.REGION}:{self.config.ACCOUNT_ID}:*"
-                ],
-            )
-        )
-
-        lambda_role.add_to_policy(
-            statement=PolicyStatement(
-                effect=Effect.ALLOW,
-                actions=["logs:CreateLogStream", "logs:PutLogEvents"],
-                resources=[
-                    f"arn:aws:logs:{self.config.REGION}:{self.config.ACCOUNT_ID}:log-group:/aws/lambda/{self.config.FUNCTION_NAME}"
-                ],
-            )
-        )
-
-        lambda_role.add_to_policy(
-            statement=PolicyStatement(
-                effect=Effect.ALLOW,
-                actions=["kinesis:PutRecord", "kinesis:PutRecords"],
-                resources=[self.config.KINESIS_ARN_OUTPUT],
-            )
-        )
-
-        if self.config.DOCKER_IMAGE_TAG and self.config.DOCKER_IMAGE_TAG:
+        # Docker
+        if self.config.DOCKER_IMAGE_REPO and self.config.DOCKER_IMAGE_TAG:
             code: DockerImageCode = DockerImageCode.from_ecr(
                 repository=Repository.from_repository_name(
                     self, "Repository", repository_name=self.config.DOCKER_IMAGE_REPO
@@ -234,7 +185,16 @@ class LambdaStack(Stack):
         else:
             code: DockerImageCode = DockerImageCode.from_image_asset(directory=".")
 
-        # create lambda function with docker
+        # environment variables for the lambda function
+        environment: dict = (
+            {} if self.config.FUNCTION_ENV is None else self.config.FUNCTION_ENV
+        )
+        environment["app_config_app_name"] = app_config.name
+        environment["app_config_environment_name"] = app_env.name
+        environment["app_config_profile_input"] = "input"
+        environment["app_config_profile_output"] = "output"
+
+        # create lambda function with docker image
         lambda_function: DockerImageFunction = DockerImageFunction(
             self,
             "LambdaFunction",
@@ -249,16 +209,41 @@ class LambdaStack(Stack):
             role=lambda_role,
         )
 
-        # add event source (kinesis)
-        lambda_function.add_event_source(
-            source=KinesisEventSource(
-                stream=kinesis, starting_position=StartingPosition.LATEST
+        # INPUT
+        if self.config.INPUT_TYPE == InputType.KINESIS:
+            arn_input: str = self.config.input_config_data["arn"]
+
+            # get kinesis reference
+            kinesis: IStream = Stream.from_stream_arn(
+                self, "KinesisInputEventSource", stream_arn=arn_input
             )
-        )
+
+            # add event source (kinesis)
+            lambda_function.add_event_source(
+                source=KinesisEventSource(
+                    stream=kinesis, starting_position=StartingPosition.LATEST
+                )
+            )
+
+        # OUTPUT
+        if self.config.OUTPUT_TYPE == OutputType.KINESIS:
+            arn_output: str = self.config.output_config_data["arn"]
+
+            # get kinesis reference
+            kinesis: IStream = Stream.from_stream_arn(
+                self, "KinesisOutputEventSource", stream_arn=arn_output
+            )
+
+            # allow lambda function to put records
+            lambda_role.add_to_policy(
+                statement=PolicyStatement(
+                    effect=Effect.ALLOW,
+                    actions=["kinesis:PutRecord", "kinesis:PutRecords"],
+                    resources=[arn_output],
+                )
+            )
+        elif self.config.OUTPUT_TYPE == OutputType.POSTGRESQL:
+            ...
 
         # TODO: better solution for tags (julia)
-
-        # PostgreSQL permission
-        # lambda_function.add_permission()
-        # TODO: allow the lambda function to write on the table in postgreSQL database
         # TODO: cookie cutter magic
